b24da0ce83c4000b4a36afaac81282f597ee8ca4
ifexxy <68091604+ifexxy@users.noreply.github.com>
2026-05-08 20:57:02 +0100

    Remove image domains from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  images: {
-    domains: ['i.imgur.com', 'i.ibb.co', 'miseducatemen.wordpress.com'],
-  },
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
